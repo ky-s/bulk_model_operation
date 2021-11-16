8a755423ec1bd67da521c25d91a75ebe1880d5a7
ky-s <kyohei_86@hotmail.com>
2021-11-16 21:01:24 +0900

    [clean] More practical

--- spec/practical_use_case_spec.rb
+++ spec/practical_use_case_spec.rb
@@ -5,45 +5,32 @@ require_relative '../lib/bulk_model_operation'
 require_relative 'mocks/user_with_error'
 
 RSpec.describe BulkModelOperation do
-  let(:bulk_model_operation) {
-    BulkModelOperation.new(model_class, attributes_list, **keyword_args)
-  }
-  let(:model_class) { UserWithError }
-
-  subject { bulk_model_operation.save_and_destroy }
-
-  let(:keyword_args) {
-    {
-      destroy_key: :delete,
-      save_validator: -> (record) {
-        if record.name == 'invalid'
-          error = ArgumentError.new('invalid data')
-          record.errors.push(error)
-          raise error
-        end
-      },
-      destroy_validator: -> (record) {
-        if record.name == 'invalid'
-          error = ArgumentError.new('cannot destroy')
-          record.errors.push(error)
-          raise error
-        end
-      }
-    }
-  }
-
   context 'valid' do
-    let(:attributes_list) {
-      [
+    it 'success' do
+      attributes_list = [
         { id: 1, name: 'John' },
         {        name: 'Bill' },
         { id: 3, name: 'Will', delete: true },
         { id: 4, name: 'Bob' , delete: true }
       ]
-    }
 
-    it 'success' do
-      subject
+      validator = -> (record) {
+        if record.name == 'invalid'
+          error = ArgumentError.new('invalid data')
+          record.errors.push(error)
+          raise error
+        end
+      }
+
+      bulk_model_operation = BulkModelOperation.new(
+        UserWithError,
+        attributes_list,
+        destroy_key: :delete,
+        save_validator: validator,
+        destroy_validator: validator
+      )
+
+      bulk_model_operation.save_and_destroy
 
       expect(bulk_model_operation.errors.size).to eq 0
 
@@ -56,8 +43,8 @@ RSpec.describe BulkModelOperation do
   end
 
   context 'invalid' do
-    let(:attributes_list) {
-      [
+    it 'has errors' do
+      attributes_list = [
         { id: 1, name: 'John'    },
         {        name: 'invalid' },
         { id: 2, name: 'Bill',    error_trigger: :save },
@@ -65,10 +52,24 @@ RSpec.describe BulkModelOperation do
         { id: 4, name: 'invalid', delete: true },
         { id: 5, name: 'Bob' ,    delete: true, error_trigger: :destroy }
       ]
-    }
 
-    it 'has errors' do
-      subject
+      validator = -> (record) {
+        if record.name == 'invalid'
+          error = ArgumentError.new('invalid data')
+          record.errors.push(error)
+          raise error
+        end
+      }
+
+      bulk_model_operation = BulkModelOperation.new(
+        UserWithError,
+        attributes_list,
+        destroy_key: :delete,
+        save_validator: validator,
+        destroy_validator: validator
+      )
+
+      bulk_model_operation.save_and_destroy
 
       expect(bulk_model_operation.errors.size).to eq 4
 
